3e15dde158440b33df503fd5e46423b2708980ce
sylviah <sylviah.rutto@gmail.com>
2019-08-28 10:48:57 +0300

    configured app in settings.py

--- Netflix/settings.py
+++ Netflix/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'moviez.apps.MoviezConfig',
+
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
